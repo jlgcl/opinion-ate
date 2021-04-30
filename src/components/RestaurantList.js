a7ba4f599a2f54efcd3728ef2c26a01dc272a862
jlgcl <57466808+jlgcl@users.noreply.github.com>
2021-04-30 15:08:23 -0400

    created connect for cthe RestaurantList component

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,4 +1,6 @@
 import {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {loadRestaurants} from '../store/restaurants/actions';
 
 export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
@@ -14,4 +16,12 @@ export const RestaurantList = ({loadRestaurants, restaurants}) => {
   );
 };
 
-export default RestaurantList;
+// Need the below code to pass in the necessary props to the component, which is DIFFERENT from unit test injections
+// Cypress E2E test will FAIL if these are not defined (though unit test will pass due to mock fn & injections).
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
+
+const mapDispatchToProps = {loadRestaurants};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
