2bc8f089a37399868059cb414c5c6497ad6199de
jlgcl <57466808+jlgcl@users.noreply.github.com>
2021-04-30 11:48:29 -0400

    refactored test code

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,34 +2,42 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
-  // *** CHECK LOADING BEHAVIOR *** //
-  it('loads restaurants on first render', () => {
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+  let loadRestaurants;
+  let context;
+
+  // Before each test for both tests
+  beforeEach(() => {
     // jest mock fn that has the name 'loadRestaurants'.
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
+    loadRestaurants = jest.fn().mockName('loadRestaurnats');
+
     // need to pass in restaurants in this test to pass the second behavior test (restaurant-display)
-    render(
+    context = render(
       <RestaurantList
         loadRestaurants={loadRestaurants}
         restaurants={restaurants}
       />,
     );
+  });
 
+  // *** CHECK LOADING BEHAVIOR *** //
+  it('loads restaurants on first render', () => {
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   // *** CHECK RESTAURANT-DISPLAY BEHAVIOR *** //
   it('displays the restaurants', () => {
-    const noop = () => {}; // no-operation function; doesn't do anything, instead of using a mock fn
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
+    // const noop = () => {}; // no-operation function; doesn't do anything, instead of using a mock fn
 
-    // destructure what's returned from the render to be tested
-    const {queryByText} = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
-    );
+    // // destructure what's returned from the render to be tested
+    // const {queryByText} = render(
+    //   <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    // );
+
+    const {queryByText} = context;
 
     expect(queryByText('Sushi Place')).not.toBeNull();
     expect(queryByText('Pizza Place')).not.toBeNull();
