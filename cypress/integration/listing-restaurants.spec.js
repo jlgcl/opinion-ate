cf7433acec557eed9af65317a5c68eac6d4f9067
jlgcl <57466808+jlgcl@users.noreply.github.com>
2021-04-30 11:18:38 -0400

    Restaurant-Display behavior test

--- cypress/integration/listing-restaurants.spec.js
+++ cypress/integration/listing-restaurants.spec.js
@@ -5,9 +5,11 @@ describe('Listing Restaurants', () => {
 
     cy.server({force404: true});
 
+    // routes to the url, then looks for saves the response as structured
     cy.route({
       method: 'GET',
-      url: 'https://outside-in-dev-api.herokuapp.com/YOUR-API-KEY/restaurants',
+      url:
+        'https://outside-in-dev-api.herokuapp.com/J7ruE5tghSu7a6wk6Yba9ZaIPlRWzGFN/restaurants',
       response: [
         {id: 1, name: sushiPlace},
         {id: 2, name: pizzaPlace},
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,11 +2,36 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
+  // *** CHECK LOADING BEHAVIOR *** //
   it('loads restaurants on first render', () => {
+    // jest mock fn that has the name 'loadRestaurants'.
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    const restaurants = [];
+    // need to pass in restaurants in this test to pass the second behavior test (restaurant-display)
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  // *** CHECK RESTAURANT-DISPLAY BEHAVIOR *** //
+  it('displays the restaurants', () => {
+    const noop = () => {}; // no-operation function; doesn't do anything, instead of using a mock fn
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    // destructure what's returned from the render to be tested
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
